fc0237a0c8d18bfc0078eb4be77e789a40364336
Darialess <blondolly@yandex.ru>
2023-02-25 23:20:53 +0300

    100деплой

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,9 +3,42 @@ from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
 from django.db import models
 
 
+class UserManager(BaseUserManager):
+    """ Manager для создания User. """
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        """ Создает и возвращает пользователя с имэйлом, паролем и именем. """
+        if username is None:
+            raise TypeError('Пользователи должны иметь имя пользователя.')
+
+        if email is None:
+            raise TypeError('пользователи должны иметь email.')
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        """ Создаёт суперпользователя. """
+        if password is None:
+            raise TypeError('У Суперпользователя должен быть пароль.')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """ Модель пользователя"""
+
     username = models.CharField(
         db_index=True,
         max_length=150,
@@ -34,11 +67,23 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name='Фамилия',
         help_text='Фамилия пользователя'
     )
+    is_active = models.BooleanField(default=True)
+
+    is_staff = models.BooleanField(default=False)
+
     is_subscribed = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
+    objects = UserManager()
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -55,17 +100,21 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 class Follow(models.Model):
+    """ Модель подписки на авторов"""
+
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='follower',
         verbose_name="Подписчик",
+        help_text='тот кто подписывается ',
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='following',
         verbose_name="Автор рецепта",
+        help_text='тот на кого подписываются',
     )
 
     class Meta:
