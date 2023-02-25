14b0cbe014a96f593f0618a9768a5a3d5db13f71
Darialess <blondolly@yandex.ru>
2023-02-25 16:46:19 +0300

    77деплой

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
+
+from users.views import CustomUserViewSet
 
 from .views import IngredientViewSet, RecipeViewSet, TagViewSet
 
+router = DefaultRouter()
 
-router = routers.DefaultRouter()
 router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('users', CustomUserViewSet, basename='users')
 router.register('tags', TagViewSet, basename='tags')
 router.register('recipes', RecipeViewSet, basename='recipes')
 
