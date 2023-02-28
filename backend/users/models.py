324a8acc46543cff463fec2e8c359d55597ef305
Darialess <blondolly@yandex.ru>
2023-03-01 00:21:53 +0300

    147деплой

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,6 +5,26 @@ from django.db import models
 
 
 class CustomUserManager(BaseUserManager):
+    def create_superuser(
+            self, email, username, first_name, last_name,
+            password, **other_fields
+    ):
+
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if not other_fields.get("is_staff"):
+            raise ValueError("Отказано в доступе")
+
+        if not other_fields.get("is_superuser"):
+            raise ValueError("Отказано в доступе")
+
+        return self.create_user(
+            email, username, first_name, last_name,
+            password=password, **other_fields
+        )
+
     def create_user(self, first_name, last_name,
                     email, password, **other_fields):
 
@@ -31,7 +51,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     email = models.EmailField(
         'Электронная почта',
-        max_length=150,
+        max_length=254,
         unique=True,
     )
     first_name = models.CharField(
@@ -42,14 +62,29 @@ class User(AbstractBaseUser, PermissionsMixin):
         'Фамилия',
         max_length=150,
     )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
-    def __str__(self):
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
         return self.username
+
+    def __str__(self):
+        return self.email
 
 
 class Follow(models.Model):
